86c9643dfca8cba6e5cc64ed0b8171ae03d3c881
tsedenbaljir <tsedenbaljir@nso.mn>
2023-06-01 16:30:54 +0800

    first commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "testdb",
+  HOST: "sql.freedb.tech",
+  USER: "freedb_mohs_root",
+  PASSWORD: "J53#8EKd$Bw@b%e",
+  DB: "freedb_mohsdb",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,3 +11,31 @@ module.exports = {
     idle: 10000
   }
 };
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "testdb",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+
+// module.exports = {
+//   HOST: "103.87.69.135",
+//   USER: "mohsuvgo_root",
+//   PASSWORD: "mohs.uv@mn",
+//   DB: "mohsuvgo_dataDB",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
--- server.js
+++ server.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 const db = require("./app/models");
 
 global.__basedir = __dirname;
 app.use(cors());
-
 // parse requests of content-type - application/json
 app.use(express.json());
-
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
-
 db.sequelize.sync();
-
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "API server." });
+  res.json({ message: "Нэгдсэн эмнэлэгийн API" });
 });
-
 // routes
 require("./app/routes/articles.routes")(app);
 require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 require('./app/routes/person.routes')(app);
 require('./app/routes/pages.routes')(app);
-
 // set port, listen for requests
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
