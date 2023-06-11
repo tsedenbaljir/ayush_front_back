1d838a85b959fcf84db933a489576dfc451c88c3
tsedenbaljir <tsedenbaljir@nso.mn>
2023-06-11 16:47:50 +0800

    new deploy

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ require('./app/routes/pages.routes')(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`:Server is running on port ${PORT}.`);
 });
