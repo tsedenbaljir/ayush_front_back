a716a7c06df8ec50da66fcaeb125341f72d10fd5
tsedenbaljir <tsedenbaljir@nso.mn>
2023-06-11 16:43:01 +0800

    new deploy

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,22 +1,8 @@
-// module.exports = {
-//   HOST: "us-cdbr-east-06.cleardb.net",
-//   USER: "b846f265e10945",
-//   PASSWORD: "17427019",
-//   DB: "heroku_2fdba4a684d2921",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-// mysql://b846f265e10945:17427019@us-cdbr-east-06.cleardb.net/heroku_2fdba4a684d2921?reconnect=true
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "testdb",
+  HOST: "us-cdbr-east-06.cleardb.net",
+  USER: "b846f265e10945",
+  PASSWORD: "17427019",
+  DB: "heroku_2fdba4a684d2921",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -25,6 +11,20 @@ module.exports = {
     idle: 10000
   }
 };
+// mysql://b846f265e10945:17427019@us-cdbr-east-06.cleardb.net/heroku_2fdba4a684d2921?reconnect=true
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "testdb",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
 
 // module.exports = {
 //   HOST: "103.87.69.135",
