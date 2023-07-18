28a388820724a4bfc46148cd7d63bb1400ae56f9
tsedenbaljir <tsedenbaljir@nso.mn>
2023-07-18 13:31:05 +0800

    prod bolson

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const db = require("./app/models");
+const https = require('https');
+
+const fs = require('fs');
+
+const options = {
+	key: fs.readFileSync('key.key'),
+	cert: fs.readFileSync('nso.pem')
+  };
 
 global.__basedir = __dirname;
 app.use(cors());
@@ -17,8 +25,7 @@ app.get("/", (req, res) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers','X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-	next();
-  res.json({ message: "Нэгдсэн эмнэлэгийн API" });
+  res.json({ message: "REST API" });
 });
 // routes
 require("./app/routes/articles.routes")(app);
@@ -27,6 +34,8 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/person.routes')(app);
 require('./app/routes/pages.routes')(app);
 // set port, listen for requests
+// https.createServer(options, app).listen(3001);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`:Server is running on port ${PORT}.`);
