2d38dde7db2e056c16287a6ddbddd2ccf1617a1e
tsedenbaljir <tsedenbaljir@nso.mn>
2024-06-06 16:23:05 +0800

    edit db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,11 +4,18 @@ module.exports = {
   PASSWORD: "",
   DB: "my_database",
   dialect: "mysql",
+  port: 3306,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000
+  },
+  dialectOptions: {
+      socketPath: "/var/run/mysqld/mysqld.sock"
+  },
+  define: {
+      paranoid: true
   }
 };
 // mysql://b846f265e10945:17427019@us-cdbr-east-06.cleardb.net/heroku_2fdba4a684d2921?reconnect=true
